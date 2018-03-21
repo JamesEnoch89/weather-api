e44da591d369f5c45c1555cc7f33d0f6fee4ad3a
Enoch <jenoch@proforma.com>
2018-03-21 16:17:02 -0400

    zip done

--- public/main.js
+++ public/main.js
@@ -1,5 +1,23 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+
+const searchWeather = () => {
+  let weatherSearch = document.querySelector("#searchZip").value;
+  console.log(weatherSearch)
+
+const zipUrl = `http://api.openweathermap.org/data/2.5/weather?zip=${weatherSearch}&units=imperial&APPID=9532a3721313153e5e16b590ba5602c2`
+
+fetch(zipUrl).then((response) => {
+  if (response.status === 200) {
+    return response.json()
+  }
+}).then((weather) => {
+  console.log(weather);
+  const output = document.ques
+
+})
 }
 
-document.addEventListener('DOMContentLoaded', main)
+// if(weatherSearch) === typeof number return ^^above applicationCache
+
+// else
+
+// city api**
