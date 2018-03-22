8e824c123cc0c7c2d7e9303155f3bdf3fb94ca50
Enoch <jenoch@proforma.com>
2018-03-21 21:50:51 -0400

    finished explorer mode

--- public/main.js
+++ public/main.js
@@ -1,23 +1,31 @@
 
 const searchWeather = () => {
   let weatherSearch = document.querySelector("#searchZip").value;
-  console.log(weatherSearch)
+  console.log(weatherSearch);
 
-const zipUrl = `http://api.openweathermap.org/data/2.5/weather?zip=${weatherSearch}&units=imperial&APPID=9532a3721313153e5e16b590ba5602c2`
+  let weatherAPI = ""
 
-fetch(zipUrl).then((response) => {
-  if (response.status === 200) {
-    return response.json()
+  if (isNaN(weatherSearch)) {
+    weatherAPI = `http://api.openweathermap.org/data/2.5/weather?q=${weatherSearch}&units=imperial&APPID=9532a3721313153e5e16b590ba5602c2`
+  } else {
+    weatherAPI = `http://api.openweathermap.org/data/2.5/weather?zip=${weatherSearch}&units=imperial&APPID=9532a3721313153e5e16b590ba5602c2`;
+  }
+
+  fetch(weatherAPI).then((response) => {
+    if (response.status === 200) {
+      return response.json()
   }
 }).then((weather) => {
   console.log(weather);
-  const output = document.ques
+  let temp = document.querySelector("#currentTemp")
+  let location = document.querySelector("#location")
+  let humidity = document.querySelector("#humidity")
+  let wind = document.querySelector("#wind")
+
+  temp.textContent = `The current temperature in ${weather.name} is: ${weather.main.temp}℉`;
+  location.textContent = `The conditions has ${weather.weather[0].description}`;
+  wind.textContent = `The wind speed is ${weather.wind.speed}`
+  humidity.textContent = `The humidity is: ${weather.main.humidity}%`;
 
 })
 }
-
-// if(weatherSearch) === typeof number return ^^above applicationCache
-
-// else
-
-// city api**
